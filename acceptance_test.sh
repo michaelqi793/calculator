06fa6c59ce187ab343afab066584d55f820fbcba
Archer <0.0.0.0>
2022-12-11 03:47:55 +0800

    # add acceptance test

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 #test $(curl http://172.17.0.5:8081/sum?a=1\&b=2) -eq 3
-test $(curl 'http://'|| $CONTAINER_IP || ':8081/sum?a=1&b=2') -eq 3
+#test $(curl 'http://' || $CONTAINER_IP || ':8081/sum?a=1&b=2') -eq 3
+curl http://'"${CONTAINER_IP}"':8081/sum?a=1\&b=2
 echo $CONTAINER_IP
