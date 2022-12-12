2f8f305aae14c696b59e6cca6d36a7354a6c71da
Archer <0.0.0.0>
2022-12-12 18:21:58 +0800

    # add cucumber test as auto user acceptance test

---
+++ src/test/java/acceptance/AcceptanceIT.java
@@ -0,0 +1,17 @@
+package acceptance;
+
+
+import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.*;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+/** Acceptance Test */
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("acceptance")
+@ConfigurationParameter(
+        key = GLUE_PROPERTY_NAME,
+        value = "acceptance"
+        )
+public class AcceptanceIT { }
