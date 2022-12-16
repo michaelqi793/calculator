06d0561778673f0869c03144bbae8609b23c89cb
Archer <0.0.0.0>
2022-12-16 14:32:37 +0800

    disable cache

--- src/main/java/com/archer/calculator/CalculatorApplication.java
+++ src/main/java/com/archer/calculator/CalculatorApplication.java
@@ -2,7 +2,6 @@ package com.archer.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 /**
  * i have to put a doc here.
