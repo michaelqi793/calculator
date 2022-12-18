e8b61487defe7d630a376040be6c45e30a9dd934
Archer <0.0.0.0>
2022-12-18 19:35:58 +0800

    add kubernetes

--- acceptance_test_kubernetes.sh
+++ acceptance_test_kubernetes.sh
@@ -3,5 +3,5 @@ set -x
 ifconfig
 NODE_IP=$(kubectl --kubeconfig=/home/kubernetes/.kube/config get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="ExternalIP")].address }')
 NODE_PORT=$(kubectl --kubeconfig=/home/kubernetes/.kube/config get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
-echo http://${NODE_IP}:${NODE_PORT}
-mvn failsafe:integration-test failsafe:verify -Dcalculator.url=http://${NODE_IP}:${NODE_PORT}
+echo http://172.17.0.1:${NODE_PORT}
+mvn failsafe:integration-test failsafe:verify -Dcalculator.url=http://172.17.0.1:${NODE_PORT}
