4f7412cf8709f5a249d96c1939d83c5176c762d4
Archer <0.0.0.0>
2022-12-18 21:26:35 +0800

    ##test against staging env

--- acceptance_test_kubernetes.sh
+++ acceptance_test_kubernetes.sh
@@ -4,4 +4,4 @@ NODE_IP=$(kubectl --kubeconfig=/home/kubernetes/.kube/config get nodes -o jsonpa
 NODE_PORT=$(kubectl --kubeconfig=/home/kubernetes/.kube/config get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
 echo http://172.17.0.1:${NODE_PORT}
 test $(curl http://172.17.0.1:${NODE_PORT}/sum?a=1\&b=2) -eq 3
-##mvn failsafe:integration-test failsafe:verify -Dcalculator.url=http://172.17.0.1:${NODE_PORT}
+mvn failsafe:integration-test failsafe:verify -Dcalculator.url=http://172.17.0.1:${NODE_PORT}
