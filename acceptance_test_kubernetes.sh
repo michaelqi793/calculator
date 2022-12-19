7b7398d04844e6da1216def5c9d3e40dd6a80504
Archer <0.0.0.0>
2022-12-19 13:33:34 +0800

    ## add releasing stage and smoke test

--- acceptance_test_kubernetes.sh
+++ acceptance_test_kubernetes.sh
@@ -3,5 +3,9 @@ set -x
 NODE_IP=$(kubectl --kubeconfig=/home/kubernetes/.kube/config get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="ExternalIP")].address }')
 NODE_PORT=$(kubectl --kubeconfig=/home/kubernetes/.kube/config get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
 echo http://172.17.0.1:${NODE_PORT}
+## repeat to test the cache
+test $(curl http://172.17.0.1:${NODE_PORT}/sum?a=1\&b=2) -eq 3
+test $(curl http://172.17.0.1:${NODE_PORT}/sum?a=1\&b=2) -eq 3
+test $(curl http://172.17.0.1:${NODE_PORT}/sum?a=1\&b=2) -eq 3
 test $(curl http://172.17.0.1:${NODE_PORT}/sum?a=1\&b=2) -eq 3
 mvn failsafe:integration-test failsafe:verify -Dcalculator.url=http://172.17.0.1:${NODE_PORT}
